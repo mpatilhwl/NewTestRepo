838ed875daccaa39da9da1c5d5a03715e8818590
Milind <mpatil@hwlat.com>
2025-07-16 15:55:22 +0530

    final working

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,21 +1,22 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './e2e',
+  testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
- 
+  
   reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+ 
   use: {
    
     trace: 'on-first-retry',
   },
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+
   ],
+
 });
